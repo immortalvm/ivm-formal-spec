01b60a5ccb243915496d89e2356b3d8603b089eb
Ivar Rummelhoff <ivarrru@gmail.com>
2020-08-16 21:51:50 +0200

    LensExtras.v updated (removed unused vector stuff)

--- assembly/LensExtras.v
+++ assembly/LensExtras.v
@@ -33,21 +33,7 @@ Section char_section.
     f_equal; apply proof_irrelevance.
    Qed.
 
-  (** Since "unit" shorter than "terminal". *)
-  #[refine] Instance unitLens : Lens A unit :=
-  {
-    proj _ := tt;
-    update a _ := a;
-  }.
-  Proof.
-    1: intros _ [].
-    all: reflexivity.
-  Defined.
-
 End char_section.
-
-
-
 
 
 (** ** Elementary *)
@@ -61,14 +47,15 @@ Section elementary_section.
     update a _ := a;
   }.
   Proof.
-    all: intro a; repeat intros []; reflexivity.
+    1: intros _ [].
+    all: reflexivity.
   Defined.
 
   Program Instance independent_unit {A X} {Lx: Lens A X} : Independent unitLens Lx.
 
   Instance unitSublens {A X} (Lx: Lens A X) : (Lx | idLens).
   Proof.
-    exists Lx. symmetry. apply idLens_composite.
+    intros a b c. reflexivity.
   Qed.
 
   #[refine] Instance falseLens {X} : Lens False X :=
@@ -87,24 +74,14 @@ Section elementary_section.
 
   Context {A X} (Lx: Lens A X).
 
-  Instance sublens_refl : (Lx | Lx).
-  Proof.
-    exists idLens. symmetry. apply composite_idLens.
-  Qed.
-
   Instance terminal_sublens : (Lx | idLens ).
   Proof.
-    exists Lx. symmetry. apply idLens_composite.
+    intros a b c. reflexivity.
   Qed.
 
   Instance initial_sublens : (unitLens | Lx).
   Proof.
-    exists unitLens.
-    intros a [].
-    cbn.
-    unfold compose.
-    rewrite update_proj.
-    reflexivity.
+    intros a b c. reflexivity.
   Qed.
 
   Context {Y} (Ly: Lens A Y) {Hi: Independent Lx Ly}.
@@ -688,124 +665,6 @@ Section sublens_section.
 End sublens_section.
 
 
-(** ** Vector lenses *)
-
-Section lens_vector_section.
-
-  Open Scope vector.
-
-  Context {A X: Type} {Lx: Lens A X} {La: Lens A A}.
-
-  Equations projN n (_: A) : vector X n :=
-    projN 0 _ := [];
-    projN (S n) a := (proj a :: projN n (proj a)).
-
-  Equations projN' `(nat) `(A) : A :=
-    projN' 0 a := a;
-    projN' (S n) a := (projN' n (proj a)).
-
-  Arguments update : clear implicits.
-  Arguments update {_ _} _ _ _.
-
-  Equations updateN {n} `(A) `(vector X n) : A :=
-    updateN (n:=S n) a (x :: r) := (update Lx (update La a (updateN (proj a) r)) x);
-    updateN a _ := a.
-
-  Equations updateN' (n:nat) `(A) `(A) : A :=
-    updateN' (S n) a a' := update La a (updateN' n (proj a) a');
-    updateN' _ _ a' := a'.
-
-  Context {IXA: Independent Lx La}.
-
-  #[refine] Instance lens_vector n : Lens A (vector X n) :=
-  {
-    proj a := projN n a;
-    update a x := updateN a x;
-  }.
-  Proof.
-    - induction n; intros a x; dependent elimination x.
-      + reflexivity.
-      + simp projN updateN.
-        rewrite proj_update. f_equal.
-        rewrite <- (IHn (proj a)). f_equal.
-        lens_rewrite.
-    - induction n; intros a.
-      + reflexivity.
-      + simp projN updateN.
-        rewrite IHn. lens_rewrite.
-    - induction n; intros a x x';
-        dependent elimination x;
-        dependent elimination x'.
-      + reflexivity.
-      + simp projN updateN.
-        lens_rewrite. rewrite IHn. reflexivity.
-  Defined.
-
-  #[refine] Instance lens_vector' n : Lens A A :=
-  {
-    proj a := projN' n a;
-    update a x := updateN' n a x;
-  }.
-  Proof.
-    - induction n; intros a x.
-      + reflexivity.
-      + simp projN' updateN'.
-        rewrite proj_update, IHn.
-        reflexivity.
-    - induction n; intros a.
-      + reflexivity.
-      + simp projN' updateN'.
-        rewrite IHn. lens_rewrite.
-    - induction n; intros a x x'.
-      + reflexivity.
-      + simp updateN'.
-        lens_rewrite. rewrite IHn. reflexivity.
-  Defined.
-
-  Instance independent_vector n : Independent (lens_vector n) (lens_vector' n).
-  Proof.
-    induction n; [intro; reflexivity|].
-    unfold Independent in IHn.
-    simpl in IHn.
-    intros a x y.
-    simpl.
-    dependent elimination x.
-    simp updateN updateN'.
-    lens_rewrite.
-    rewrite IHn.
-    reflexivity.
-  Qed.
-
-  Existing Instance prodLens.
-  Context (Bp: Bijection_lens (Lx * La)).
-
-  Equations inverseN {n} `(vector X n) `(A) : A :=
-    inverseN (n:=S n) (x :: r) a := inverse (x, inverseN r a);
-    inverseN _ a := a.
-
-  #[refine] Instance bijection_vector n : Bijection_lens ((lens_vector n) * (lens_vector' n)) :=
-    bijection_lens (fun va => inverseN (fst va) (snd va)) _.
-  Proof.
-    intros a [v a']. simpl. revert a v a'.
-    induction n; intros a v a'; dependent elimination v; simp inverseN.
-    - reflexivity.
-    - simp updateN' updateN.
-      lens_rewrite.
-      rewrite IHn.
-      rewrite <- (update_as_inverse a).
-      simpl.
-      lens_rewrite.
-  Defined.
-
-End lens_vector_section.
-
-Arguments lens_vector : clear implicits.
-Arguments lens_vector {_ _} _ _ {_} _.
-
-Arguments lens_vector' : clear implicits.
-Arguments lens_vector' {_} _ _.
-
-
 (** ** Every lens is a product lens
 
 Assuming functional extensionality and proof irrelevance, we have a
@@ -841,8 +700,7 @@ Section Inv_lens.
 
   Instance inv_lens_independent : Independent inv_lens PX.
   Proof.
-    intros s [f Hf] x. simpl.
-    rewrite proj_update, Hf. reflexivity.
+    intros a b c. cbn. lens_rewrite.
   Qed.
 
   Lemma inv_lens_inv (s: S) :
@@ -853,19 +711,3 @@ Section Inv_lens.
   Qed.
 
 End Inv_lens.
-
-
-(** ** Various *)
-
-Instance sublens_independent
-         {A X Y}
-         {Lx Lx': Lens A X} {Hx: (Lx | Lx')}
-         {Ly Ly': Lens A Y} {Hy: (Ly | Ly')}
-         {Hxy: Independent Lx' Ly'} : Independent Lx Ly.
-Proof.
-  destruct Hx as [Lxx Hxx].
-  destruct Hy as [Lyy Hyy].
-  rewrite Hxx.
-  rewrite Hyy.
-  typeclasses eauto.
-Qed.
